aa386ee64deca58b2361bef1b09d546a6ddcf6d6
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-09-24 15:33:02 +0700

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
+
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
